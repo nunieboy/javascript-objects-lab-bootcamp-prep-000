a6fb55b158f93e90a34111eade1e543cc4d0e0b9
Pablo Eusebio <nunieboy88@gmail.com>
2018-08-29 13:53:29 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 var recipes = {}
 
-function uipdateObjectwithKeyAndValue(obj, key, value){
+function updateObjectwithKeyAndValue(obj, key, value){
   var newObj = Object.assign({}, {key: value})
   return newObj
 }
