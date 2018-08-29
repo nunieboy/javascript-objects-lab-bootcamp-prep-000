a0c649d559e0de702acc1794a54ecc6d4b43adcf
Pablo Eusebio <nunieboy88@gmail.com>
2018-08-29 14:23:35 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 var recipes = {flour: "1 cup", milk: "1 cup", sugar: "1 tbsp"};
 
 function updateObjectWithKeyAndValue(obj, key, value){
-  return Object.assign({}, {key: value})
+  var Object.assign({}, {key: value})
   return newObj
 }
